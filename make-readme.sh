fd022b25b57d686c16432e45a11bc10b6c08adcc
Greg Werbin <gw2286@columbia.edu>
2014-05-01 13:07:23 -0400

    Fancy readme, now without typos

--- make-readme.sh
+++ make-readme.sh
@@ -1,6 +1,6 @@
 #!C:\cygwin64\bin\bash.exe
 
-code=$(cat easy_ezproxy_readable.js | tr -d '\011\012\015' | C:/Users/Greg/utf8.pl)
 protocol="javascript:"
+code=$(cat easy_ezproxy_readable.js | tr -d '\011\012\015' | C:/Users/Greg/utf8.pl)
 
-sed 8s/.*/${code}${protocol}/ README_empty.md > README.md
+sed 8s/.*/$protocol$code/ README_empty.md > README.md
